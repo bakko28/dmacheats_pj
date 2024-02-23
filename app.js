c4138d0cf82836405e5c1bd4ee9aa4e7c9604eb9
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-02-23 13:14:52 +0300

    catalog done

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('catalogBtn').onclick = function() {
+    window.location.href = 'catalog.html';
+};
+
+document.getElementById('mainPageBtn').onclick = function() {
+    window.location.href = 'index.html';
+};
