25ff8571f2c26f083979e0815d1fa06a948f1b1c
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-02-24 03:22:50 +0300

    done 3

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-document.getElementById('catalogBtn').onclick = function() {
-    window.location.href = 'catalog.html';
-};
+const catalogBtn = document.querySelectorAll('#catalogBtn');
+const mainBtn = document.querySelectorAll('#mainPageBtn');
 
-document.getElementById('mainPageBtn').onclick = function() {
-    window.location.href = 'index.html';
-};
+catalogBtn.forEach(btn => {
+    btn.onclick = function() {
+        window.location.href = 'catalog.html';
+    };
+});
+
+mainBtn.forEach(btn => {
+    btn.onclick = function () {
+        window.location.href = 'index.html';
+    };
+});
+
+const buyBtns = document.querySelectorAll('#buyBtn');
+
+buyBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let targetSection = document.querySelector('.application-box');
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            console.error('Целевой раздел не найден');
+        }
+    });
+});
+
+const technologyBtns = document.querySelectorAll('#technologyBtn');
+
+technologyBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let targetSection = document.querySelector('.technology-box');
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            console.error('Целевой раздел не найден');
+        }
+    });
+});
+
+const deliveryBtns = document.querySelectorAll('#deliveryBtn');
+
+deliveryBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let targetSection = document.querySelector('.delivery-box');
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            console.error('Целевой раздел не найден');
+        }
+    });
+});
